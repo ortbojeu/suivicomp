8821e65ff0910f2e8a0970ca2cd5fd72fb3fdd89
moiroud thibaut <moiroud thibaut>
2017-05-11 15:46:11 +0200

    Modif template de base pour les classes Model

--- src/AppBundle/Model/Default.class.php
+++ src/AppBundle/Model/Default.class.php
@@ -4,12 +4,22 @@ namespace AppBundle\Model;
 
 class Class {
 
-  function __construct() {
-    // CODE
-  }
+  // VARIABLES
+  private $var;
 
+  // CONSTRUCTOR
+  public function __construct() {}
+
+  // GETTER & SETTER
+  public function getVar() { return $this->var; }
+  public function setVar($var) { $this->var = $var; }
+
+  // TOOL FUNCTIONS
   public function toString() {
-    return "";
+    $str = "Class - [ ";
+    $str .= "var : " . ((isset($this->getVar()) && $this->getVar() != null) ? $this->getVar() : "");
+    $str .= " ]";
+    return $str;
   }
 
 }
